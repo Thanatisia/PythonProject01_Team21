4bb0e012b19913cf1dee5b57b98e01caab7eeb48
weihenglim <imweihengsit@gmail.com>
2019-09-25 17:15:44 +0800

    Add comments to main.py

--- dataset_analyzer/main.py
+++ dataset_analyzer/main.py
@@ -1,7 +1,9 @@
 import io_handler
 
+# Get user to input the file path of data sets
 file_1_path = raw_input("Enter the file path of data set 1: ")
 file_2_path = raw_input("Enter the file path of data set 2: ")
 
+# Store data into dictionary lists
 data_set_1 = io_handler.read_csv(file_1_path)
 data_set_2 = io_handler.read_csv(file_2_path)
