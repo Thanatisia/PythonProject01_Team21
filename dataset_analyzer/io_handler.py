a7b837fec7f29ce8cf739228509765814c1f8474
weihenglim <imweihengsit@gmail.com>
2019-09-25 17:04:19 +0800

    Add io_handler.py module

---
+++ dataset_analyzer/io_handler.py
@@ -0,0 +1,13 @@
+"""
+Module that includes functions that handle input/output operations
+"""
+
+
+# Reads a csv file and stores it in a dictionary list
+def read_csv(_file_path):
+
+    output_list = []
+
+    # TODO: Implement read_csv() function
+
+    return output_list
